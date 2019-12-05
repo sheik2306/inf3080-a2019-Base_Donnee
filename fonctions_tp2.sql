55f050433cc281eeec3e0976ea46141df31a8c96
sheik2306 <alex.dufour@outlook.com>
2019-12-05 18:40:44 -0500

    complete tp

--- fonctions_tp2.sql
+++ fonctions_tp2.sql
@@ -34,21 +34,7 @@ end plusLongtrajet;
 
 
 
--- TEST DE PLUS LONGUE DISTANCE --
 
-SELECT
-     route.NDISTANCE   
-    FROM  chargement,  route , soumissione e
-    WHERE chargement.pchargement = e.pchargement 
-    AND chargement.nlatori = route.nlatori
-    AND chargement.nlongori = route.nlongori
-    AND chargement.nlatdes = route.nlatdes
-    AND chargement.nlongdes = route.nlongdes   
-    and e.datesoumission = '17-08-18';
-    /
-
-select distinct plusLongTrajet('17-08-18') from soumissione;
-/
 
 
 
@@ -67,34 +53,61 @@ is
 prixTotal  Number(8,2);
 n_distance route.ndistance%TYPE;
 n_cout Carburant.nCout%Type;
+n_coutE TypeEquipement.ncout%type;
 
 BEGIN 
 
 SELECT 
-route.ndistance, Carburant.ncout
-into n_distance ,n_cout
+route.ndistance, Carburant.ncout, TypeEquipement.ncout
+into n_distance ,n_cout, n_coutE
  FROM    Chargement, route ,
- soumissione , soumissiond , Camion, Tracteur, carburant
+ soumissione , soumissiond , Camion, Tracteur, carburant, equipement,TypeEquipement
 
  WHERE soumissiond.psoumissiond = noSoumission
- AND soumissiond.psoumissione = soumissione.psoumissione
+    AND soumissiond.psoumissione = soumissione.psoumissione
     AND chargement.pchargement = soumissione.pchargement
     AND n_latOri = route.nlatori
     AND n_longori = route.nlongori
     AND n_latdes = route.nlatdes
     AND n_longdes = route.nlongdes 
     AND camion.camion_id = soumissiond.camion_id
+    and camion.pequipement = Equipement.pequipement
+    and equipement.ptypeequipement = TypeEquipement.ptypeequipement
     AND Tracteur.ptracteur = Camion.pTracteur
     AND Carburant.pCarburant = Tracteur.pCarburant;
 
+ dbms_output.put_line( 'Distance a parcourir' ||  n_distance);
+ dbms_output.put_line( 'Cout de carburant' ||  n_cout);
+ dbms_output.put_line( 'Cout dequipement' ||  n_coutE);
 
-prixTotal:=n_distance*n_cout;
+prixTotal:=(n_distance*n_cout) + n_coutE; --Cout carburant * Distance
 
 return (prixTotal);
 END coutTotalTrajet;
 /
-        
-Select distinct couttotalTrajet(1,65.3,-75.4,44.3,-78.4) FROM soumissiond;
+
+SET ECHO ON
+SET SERVEROUTPUT ON
+CREATE OR REPLACE FUNCTION totalFacture(no_facture Facture.nofacture%TYPE)
+return number
+IS
+prixTotal Facture.nTotal%TYPE;
+
+BEGIN
+
+select
+    ntotal
+into prixTotal
+FROM
+    FACTURE
+WHERE
+    FACTURE.nofacture = no_facture;
+    
+    return(prixTotal);
+
+end totalFacture;
 /
 
+select distinct totalFacture(2) from facture;
+/
 
